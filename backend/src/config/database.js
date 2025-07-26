efc9811d1b2e05785d4414d6c481e22e9f8ffc5c
T Warfield-Graham <twarfield-graham@Mac.lan>
2025-07-26 17:04:57 -0600

    Adds server.js as Express app entry point

---
+++ backend/src/config/database.js
@@ -0,0 +1,53 @@
+// backend/src/config/database.js
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config({ path: '../.env' }); // double check the path to your .env file
+
+const sequelize = new Sequelize( process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: true, // See queries in the console
+});
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+sku: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2)  // Adjust precision and scale as needed
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+    },
+    category: {
+        type: DataTypes.STRING,
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+    },
+}, {
+    tableName: 'products',
+    timestamps: true, // Adds createdAt and updatedAt fields
+});
+    
+module.exports = {
+    sequelize,
+    Product,
+};
